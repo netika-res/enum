810654ec92548bbfc5f1fa055d312b331519e050
Corentin Lamy <corentin.lamy@netika.com>
2019-07-18 17:08:22 +0200

    Change Version for build

--- lib/enum/base.rb
+++ lib/enum/base.rb
@@ -82,7 +82,8 @@ module Enum
         @history = set
       end
 
-=begin        def translate(token, options = {})
+=begin
+      def translate(token, options = {})
         I18n.t(token, scope: "enum.#{self}", exception_handler: proc do
           if superclass == Enum::Base
             I18n.t(token, options.merge(scope: "enum.#{self}"))
